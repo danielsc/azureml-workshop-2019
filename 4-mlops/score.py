c2f161a23efc1930dd489c85dff647b1e4f75e98
aashishb <aashishb@microsoft.com>
2019-10-18 14:20:51 -0700

    Update score.py

--- 4-mlops/score.py
+++ 4-mlops/score.py
@@ -19,7 +19,7 @@ def init():
     global model
     # This name is model.id of model that we want to deploy deserialize the model file back
     # into a sklearn model
-    model_path = Model.get_model_path('IBM-attrition-model')
+    model_path = Model.get_model_path('IBM_attrition_model')
     model = joblib.load(model_path)
 
 
