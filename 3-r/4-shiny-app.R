0771026b3ab6b8f3ef918aebc1481c4dbe9a118d
Ubuntu <azureuser@danielsc0ee78832172c.4jrh4w3if2be5m5o33gy45lhxb.cbnx.internal.cloudapp.net>
2019-10-20 01:35:50 +0000

    shiny app

---
+++ 3-r/4-shiny-app.R
@@ -0,0 +1,69 @@
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+library(ggplot2)
+library(shiny)
+library(azuremlsdk)
+library(jsonlite)
+library(data.table)
+
+ws <- load_workspace_from_config()
+all_data <- fread('../data/IBM-Employee-Attrition.csv',stringsAsFactors = TRUE)
+# remove useless fields 
+all_data = within(all_data, rm(EmployeeCount, Over18, StandardHours, EmployeeNumber))
+# make sure attrition is a factor
+for (col in c('Attrition')) 
+  set(all_data, j=col, value=as.factor(all_data[[col]]))
+
+# get service from the workspace to refresh the object
+service = ws$webservices$attritionr
+
+
+# Define UI for slider demo app ----
+ui <- fluidPage(
+  titlePanel("Score AzureML Model"),
+  
+  # Create a new row for the table.
+  DT::dataTableOutput("table"),
+  h4("JSON sent to service"),
+  verbatimTextOutput("json"),
+  h4("Result returned by service"),
+  verbatimTextOutput('result')
+)
+
+# Define server logic for slider examples ----
+server <- function(input, output) {
+  
+  # Filter data based on selections
+  output$table <- DT::renderDataTable(all_data,
+                                      selection = 'single')
+  
+  output$json <- renderText({
+    if (is.null(input$table_rows_selected)) {
+      "select a table row"
+    } else {
+      sample = all_data[input$table_rows_selected]
+      sample$Attrition = NULL
+      toJSON(sample)
+    }
+  })
+  
+  output$result <- renderText({
+    if (is.null(input$table_rows_selected)) {
+      "select a table row"
+    } else {
+      sample = all_data[input$table_rows_selected]
+      sample$Attrition = NULL
+      result = fromJSON(invoke_webservice(service, toJSON(sample)))
+      result
+    }
+  })
+  
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
+
+
+
+
