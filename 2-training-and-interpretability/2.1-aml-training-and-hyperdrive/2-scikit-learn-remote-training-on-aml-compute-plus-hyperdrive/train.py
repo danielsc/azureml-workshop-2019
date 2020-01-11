60a8c4a41c1c79d2568ede0320548c04a69793d5
Cesar De la Torre <cesardl@microsoft.com>
2020-01-11 11:44:46 -0800

    Updated to latesst Cesar notebooks versions

--- 2-training-and-interpretability/2.1-aml-training-and-hyperdrive/2-scikit-learn-remote-training-on-aml-compute-plus-hyperdrive/train.py
+++ 2-training-and-interpretability/2.1-aml-training-and-hyperdrive/2-scikit-learn-remote-training-on-aml-compute-plus-hyperdrive/train.py
@@ -1,4 +1,5 @@
 import joblib
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
@@ -6,25 +7,44 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn_pandas import DataFrameMapper
 import os
+import sklearn_pandas
 import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+import azureml.core
+
+import argparse
+from azureml.core import Run, Workspace, Experiment
 
 os.system('pip freeze')
 
-# +
-from azureml.core import Run, Workspace, Experiment
-# Check core SDK version number
-import azureml.core
-
-print("SDK version:", azureml.core.VERSION)
-# -
-
-OUTPUT_DIR='./outputs'
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-
-# +
-print('load dataset')
-
+# If you would like to use Azure ML's tracking and metrics capabilities, you will have to add Azure ML code inside your training script:
+# In this train.py script we will log some metrics to our Azure ML run. To do so, we will access the Azure ML Run object within the script:
+from azureml.core.run import Run
 run = Run.get_context()
+
+# Check library versions
+print("SDK version:", azureml.core.VERSION)
+print('The scikit-learn version is {}.'.format(sklearn.__version__))
+print('The joblib version is {}.'.format(joblib.__version__))
+print('The pandas version is {}.'.format(pd.__version__))
+print('The sklearn_pandas version is {}.'.format(sklearn_pandas.__version__))
+
+# Get Script Arguments for the hyper-parameters
+parser = argparse.ArgumentParser(description="Training Script")
+parser.add_argument('--solver', type=str, default='lbfgs',
+                    help='Algorithm to use for the ML task')
+parser.add_argument('--penalty', type=str, default='l2',
+                    help='Used to specify the norm used in the penalization')
+
+args, leftovers = parser.parse_known_args()
+# args = parser.parse_args()
+    
+print('The Scikit-Learn algorithm to use is {}.'.format(np.str(args.solver)))
+print('The penalty used is {}.'.format(np.str(args.penalty)))
+      
+print('Loading dataset')
 if(run.id.startswith("OfflineRun")):
     ws = Workspace.from_config()
     experiment = Experiment(ws, "Train-Explain-Interactive")
@@ -37,6 +57,7 @@ else:
     is_remote_run = True
 
 print(attritionData.head())
+
 # --- Data cleaning -----------------------------------------------
 
 # Dropping Employee count as all values are 1 and hence attrition is independent of this feature
@@ -76,9 +97,10 @@ transformations_pipeline = numeric_transformations + categorical_transformations
 # Append classifier algorithm to preprocessing pipeline.
 # Now we have a full prediction pipeline.
 model_pipeline = Pipeline(steps=[('preprocessor', DataFrameMapper(transformations_pipeline)),
-                                 ('classifier', LogisticRegression(solver='lbfgs'))])
+                                 ('classifier', LogisticRegression(solver=args.solver, penalty=args.penalty))])
 
-# (CDLTLL) Why are we using a LogisticRegression here instead of SVC?
+# Check Scikit-Learn docs to see the hyper-parameters available for the LogisticRegression:
+# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
 
 
 # +
@@ -92,29 +114,59 @@ x_train, x_test, y_train, y_test = train_test_split(attritionXData,
 
 
 # +
-# write x_test out as a pickle file for later visualization
+
+OUTPUT_DIR='./outputs'
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+# write y_test out (placed into /outputs) as a pickle file for later external usage
+y_test_pkl = 'y_test.pkl'
+joblib.dump(value=y_test, filename=os.path.join(OUTPUT_DIR, y_test_pkl))
+
+# write x_test out (placed into /outputs) as a pickle file for later external usage
 x_test_pkl = 'x_test.pkl'
-
 joblib.dump(value=x_test, filename=os.path.join(OUTPUT_DIR, x_test_pkl))
-run.upload_file('x_test_ibm.pkl', os.path.join(OUTPUT_DIR, x_test_pkl))
+# (CDLTLL No needed) run.upload_file('x_test_ibm.pkl', os.path.join(OUTPUT_DIR, x_test_pkl))
 
-print('train model')
+print('Training the model')
 # preprocess the data and train the classification model
-model_pipeline.fit(x_train, y_train)
-model = model_pipeline.steps[-1][1]
+trained_model = model_pipeline.fit(x_train, y_train)
 
-
+# (CDLTLL - Not used)
+# model = model_pipeline.steps[-1][1]
 
 # +
-# save model for use outside the script
-model_file_name = 'log_reg.pkl'
-joblib.dump(value=model_pipeline, filename=os.path.join(OUTPUT_DIR, model_file_name))
+# Calculate Accuracy for x_test
+
+# Make Multiple Predictions
+y_predictions = trained_model.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_predictions)
+# (Method 2 with svm model) accuracy = svm_model_linear.score(x_test, y_test)
+
+print('Accuracy of LogisticRegression classifier on test set: {:.2f}'.format(accuracy))
+
+# Set Accuracy metric to the run.log to be used later by Azure ML's tracking and metrics capabilities
+run.log("Accuracy", float(accuracy))
+
+# creating a confusion matrix
+cm = confusion_matrix(y_test, y_predictions)
+print(cm)
+
+# Save the model as .pkl file
+
+# +
+# save model file to the outputs/ folder to use outside the script
+model_file_name = 'classif-empl-attrition.pkl'
+joblib.dump(value=trained_model, filename=os.path.join(OUTPUT_DIR, model_file_name))
+
+# Upload model .pkl file to the ROOT folder
+# run.upload_file('model_copy.pkl', os.path.join(OUTPUT_DIR, model_file_name))
 
 # register the model with the model management service for later use
-run.upload_file('original_model.pkl', os.path.join(OUTPUT_DIR, model_file_name))
-original_model = run.register_model(model_name='amlcompute_deploy_model',
-                                    model_path='original_model.pkl')
-
+# (CDLTLL) Not needed here. I can register the model after the remote run, from the outside, depending on the results 
+#
+# original_model = run.register_model(model_name='classif-empl-attrition-aml-comp',
+#                                     model_path=os.path.join(OUTPUT_DIR, model_file_name))
 
 # -
 
