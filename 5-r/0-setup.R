4a60cc68237847ae62860f8433bfc66486e13e83
mx-iao <33701412+mx-iao@users.noreply.github.com>
2020-02-12 17:17:35 -0800

    Update 0-setup.R

--- 5-r/0-setup.R
+++ 5-r/0-setup.R
@@ -8,10 +8,14 @@
 # Install the azuremlsdk package
 install.packages('remotes')
 
-remotes::install_cran('azuremlsdk', repos = 'https://cloud.r-project.org/')
+remotes::install_github('https://github.com/Azure/azureml-sdk-for-r')
 
 # Install additional packages that will be used in this module
-install.packages(c('data.table', 'caret','kernlab','e1071'))
+install.packages(c('data.table'))
+
+# Only install the following packages if you want to run the training script train.R locally
+# You will not need these packages if you run train.R on your remote AmlCompute cluster
+install.packages(c('caret','kernlab','e1071'))
 
 # Use the azuremlsdk library to install the Python SDK
 azuremlsdk::install_azureml(envname = 'r-reticulate')
